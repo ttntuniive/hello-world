14f1f135293cc0ce45d45372b9eff68b3d519362
ttntuniive <ttnt.uniive@gmail.com>
2015-01-18 10:49:29 +0900

    all sample

---
+++ js/sample.js
@@ -0,0 +1,12 @@
+
+function firstCtrl($scope){
+  $scope.hello = "Hello Angular!";
+};
+ 
+function secondCtrl($scope){
+  $scope.hello = "Hello Monaca!";
+};
+ 
+function thirdCtrl($scope){
+  $scope.hello = "Hello World!";
+};
